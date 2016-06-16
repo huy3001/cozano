53d98f96cabf0a179f9fde364b0943f385a59508
System Administrator <root@Phucs-Macbook.local>
2016-06-17 00:40:52 +0700

    install wooecommerce and men page

---
+++ wp-content/themes/sutunam/men.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chutienphuc
+ * Date: 23/09/2015
+ * Time: 10:47
+ * Template Name: Men Page
+ */
+get_header(); ?>
+<?php
+$jk_options = get_option('redux_demo');
+?>
+<div class="left-menu">
+    <?php sutunam_menu('left-menu'); ?>
+</div>
+<div class="right-content">
+    <aside class="upsbar">
+        <ul class="upsbar-list">
+            <a class="upsbar-link" href="<?php echo $jk_options['overview_url'] ?>" target="_self">
+                <li class="upsbar-item"><?php echo $jk_options['overview_content'] ?></li>
+            </a>
+            <a class="upsbar-link" href="<?php echo $jk_options['overview_url_2'] ?>" target="_self">
+                <li class="upsbar-item"><?php echo $jk_options['overview_content_2'] ?></li>
+            </a>
+            <a class="upsbar-link" href="<?php echo $jk_options['overview_url_3'] ?>" target="_self">
+                <li class="upsbar-item"><?php echo $jk_options['overview_content_3'] ?></li>
+            </a>
+        </ul>
+    </aside>
+    <div class="row">
+        <div class="men-block content-block">
+            <img src="<?php echo $jk_options['men_img']['url'] ?>" alt="" />
+            <div class="content-block-body">
+                <?php echo $jk_options['men_desc']?>
+                <a href="<?php echo $jk_options['men_url']?>" target="_self">
+                    <span>get yours</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="women-block content-block">
+            <img src="<?php echo $jk_options['women_img']['url'] ?>" alt="" />
+            <div class="content-block-body">
+                <?php echo $jk_options['women_desc']?>
+                <a href="<?php echo $jk_options['women_url']?>" target="_self">
+                    <span>get women</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="shoes-block content-block">
+            <img src="<?php echo $jk_options['shoes_img']['url'] ?>" alt="" />
+            <div class="content-block-body">
+                <?php echo $jk_options['shoes_desc']?>
+                <a href="<?php echo $jk_options['shoes_url']?>" target="_self">
+                    <span>get shoes</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="bigsize-block content-block">
+            <img src="<?php echo $jk_options['bigsize_img']['url'] ?>" alt="" />
+            <div class="content-block-body">
+                <?php echo $jk_options['bigsize_desc']?>
+                <a href="<?php echo $jk_options['bigsize_url']?>" target="_self">
+                    <span>get bigsize</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="touch-block content-block">
+            <img src="<?php echo $jk_options['touch_img']['url'] ?>" alt="" />
+            <div class="content-block-body">
+                <?php echo $jk_options['touch_desc']?>
+                <a href="<?php echo $jk_options['touch_url']?>" target="_self">
+                    <span>get touch</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <?php get_footer(); ?>

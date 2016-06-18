fcac4482ca018326fb8e9770db2986152df498be
Huy <huy3001@gmail.com>
2016-06-18 17:57:42 +0700

    Style home page

--- wp-content/themes/sutunam/footer.php
+++ wp-content/themes/sutunam/footer.php
@@ -13,72 +13,74 @@ if (!isset($jk_options['copy_text'])) {
 } else
     $copy_text = $jk_options['copy_text'];
 ?>
-<div class="newsletter-footer col-lg-3 col-md-12">
-    <div class="newsletter">
-        <?php echo  do_shortcode('[contact-form-7 id="286" title="new-contact-us-footer" html_class="use-floating-validation-tip"]') ?>
-    </div>
+<div class="newsletter-footer">
+	<div class="container">
+		<div class="newsletter">
+			<?php echo  do_shortcode('[contact-form-7 id="286" title="new-contact-us-footer" html_class="use-floating-validation-tip"]') ?>
+		</div>
+	</div>
 </div>
 <footer id="footer">
-    <div class="container">
-        <div class="footer-menu">
-            <?php dynamic_sidebar('footer-sidebar');?>
-        </div>
-    </div>
-    <div class="container social-block">
-        <?php if($jk_options['face_link']):?>
-            <a href="<?php echo $jk_options['face_link']?>">
-                <i class="facebook"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['twit_link']):?>
-            <a href="<?php echo $jk_options['twit_link']?>">
-                <i class="twitter"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['youtube_link']):?>
-            <a href="<?php echo $jk_options['youtube_link']?>">
-                <i class="youtube"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['gmail_link']):?>
-            <a href="<?php echo $jk_options['gmail_link']?>">
-                <i class="gmail"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['instagram_link']):?>
-            <a href="<?php echo $jk_options['instagram_link']?>">
-                <i class="instergram"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['pinterest_link']):?>
-            <a href="<?php echo $jk_options['pinterest_link']?>">
-                <i class="pinterest"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['google_link']):?>
-            <a href="<?php echo $jk_options['google_link']?>">
-                <i class="google"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['linkin_link']):?>
-            <a href="<?php echo $jk_options['linkin_link']?>">
-                <i class="linkin"></i>
-            </a>
-        <?php endif;?>
-        <?php if($jk_options['tumblr_link']):?>
-            <a href="<?php echo $jk_options['tumblr_link']?>">
-                <i class="tumblr"></i>
-            </a>
-        <?php endif;?>
-    </div>
-    <div class="container payment-block">
-        <a class="payment-method mastercard" href="">Mastercard</a>
-        <a class="payment-method visa" href="">Visa</a>
-        <a class="payment-method paypal" href="">Paypal</a>
-    </div>
-    <div class="copyright container">
-        <div class="copy-title"><?php echo $copy_text;?></div>
-    </div>
+	<div class="container">
+		<div class="footer-menu">
+			<?php dynamic_sidebar('footer-sidebar');?>
+		</div>
+		<div class="social-block">
+			<?php if($jk_options['face_link']):?>
+				<a href="<?php echo $jk_options['face_link']?>">
+					<i class="facebook"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['twit_link']):?>
+				<a href="<?php echo $jk_options['twit_link']?>">
+					<i class="twitter"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['youtube_link']):?>
+				<a href="<?php echo $jk_options['youtube_link']?>">
+					<i class="youtube"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['gmail_link']):?>
+				<a href="<?php echo $jk_options['gmail_link']?>">
+					<i class="gmail"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['instagram_link']):?>
+				<a href="<?php echo $jk_options['instagram_link']?>">
+					<i class="instergram"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['pinterest_link']):?>
+				<a href="<?php echo $jk_options['pinterest_link']?>">
+					<i class="pinterest"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['google_link']):?>
+				<a href="<?php echo $jk_options['google_link']?>">
+					<i class="google"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['linkin_link']):?>
+				<a href="<?php echo $jk_options['linkin_link']?>">
+					<i class="linkin"></i>
+				</a>
+			<?php endif;?>
+			<?php if($jk_options['tumblr_link']):?>
+				<a href="<?php echo $jk_options['tumblr_link']?>">
+					<i class="tumblr"></i>
+				</a>
+			<?php endif;?>
+		</div>
+		<div class="payment-block">
+			<a class="payment-method mastercard" href="">Mastercard</a>
+			<a class="payment-method visa" href="">Visa</a>
+			<a class="payment-method paypal" href="">Paypal</a>
+		</div>
+		<div class="copyright">
+			<div class="copy-title"><?php echo $copy_text;?></div>
+		</div>
+	</div>
 </footer>
 </div> <!-- end right-content -->
 </div> <!-- end #container -->
--- wp-content/themes/sutunam/men.php
+++ wp-content/themes/sutunam/men.php
@@ -14,79 +14,82 @@ $jk_options = get_option('redux_demo');
     <?php sutunam_menu('left-menu'); ?>
 </div>
 <div class="right-content">
-	<aside class="upsbar">
-		<ul class="upsbar-list">
-			<li class="upsbar-item">
-				<a class="upsbar-link" href="<?php echo $jk_options['overview_url'] ?>" target="_self">
-					<?php echo $jk_options['overview_content'] ?>
-				</a>
-			</li>
-			<li class="upsbar-item">
-				<a class="upsbar-link" href="<?php echo $jk_options['overview_url_2'] ?>" target="_self">
-					<?php echo $jk_options['overview_content_2'] ?>
-				</a>
-			</li>
-			<li class="upsbar-item">
-				<a class="upsbar-link" href="<?php echo $jk_options['overview_url_3'] ?>" target="_self">
-					<?php echo $jk_options['overview_content_3'] ?>
-				</a>
-			</li>
-		</ul>
-	</aside>
-    <div class="row">
-        <div class="men-block content-block">
-            <img src="<?php echo $jk_options['men_img']['url'] ?>" alt="" />
-            <div class="content-block-body">
-                <?php echo $jk_options['men_desc']?>
-                <a href="<?php echo $jk_options['men_url']?>" target="_self">
-                    <span>get yours</span>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="women-block content-block">
-            <img src="<?php echo $jk_options['women_img']['url'] ?>" alt="" />
-            <div class="content-block-body">
-                <?php echo $jk_options['women_desc']?>
-                <a href="<?php echo $jk_options['women_url']?>" target="_self">
-                    <span>get women</span>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="shoes-block content-block">
-            <img src="<?php echo $jk_options['shoes_img']['url'] ?>" alt="" />
-            <div class="content-block-body">
-                <?php echo $jk_options['shoes_desc']?>
-                <a href="<?php echo $jk_options['shoes_url']?>" target="_self">
-                    <span>get shoes</span>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="bigsize-block content-block">
-            <img src="<?php echo $jk_options['bigsize_img']['url'] ?>" alt="" />
-            <div class="content-block-body">
-                <?php echo $jk_options['bigsize_desc']?>
-                <a href="<?php echo $jk_options['bigsize_url']?>" target="_self">
-                    <span>get bigsize</span>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="touch-block content-block">
-            <img src="<?php echo $jk_options['touch_img']['url'] ?>" alt="" />
-            <div class="content-block-body">
-                <?php echo $jk_options['touch_desc']?>
-                <a href="<?php echo $jk_options['touch_url']?>" target="_self">
-                    <span>get touch</span>
-                </a>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<aside class="upsbar">
+				<ul class="upsbar-list">
+					<li class="upsbar-item">
+						<a class="upsbar-link" href="<?php echo $jk_options['overview_url'] ?>" target="_self">
+							<?php echo $jk_options['overview_content'] ?>
+						</a>
+					</li>
+					<li class="upsbar-item">
+						<a class="upsbar-link" href="<?php echo $jk_options['overview_url_2'] ?>" target="_self">
+							<?php echo $jk_options['overview_content_2'] ?>
+						</a>
+					</li>
+					<li class="upsbar-item">
+						<a class="upsbar-link" href="<?php echo $jk_options['overview_url_3'] ?>" target="_self">
+							<?php echo $jk_options['overview_content_3'] ?>
+						</a>
+					</li>
+				</ul>
+			</aside>
+		</div>
+		<div class="row">
+			<div class="men-block content-block">
+				<img src="<?php echo $jk_options['men_img']['url'] ?>" alt="" />
+				<div class="content-block-body">
+					<?php echo $jk_options['men_desc']?>
+					<a href="<?php echo $jk_options['men_url']?>" target="_self">
+						<span>get yours</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="women-block content-block">
+				<img src="<?php echo $jk_options['women_img']['url'] ?>" alt="" />
+				<div class="content-block-body">
+					<?php echo $jk_options['women_desc']?>
+					<a href="<?php echo $jk_options['women_url']?>" target="_self">
+						<span>get women</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="shoes-block content-block">
+				<img src="<?php echo $jk_options['shoes_img']['url'] ?>" alt="" />
+				<div class="content-block-body">
+					<?php echo $jk_options['shoes_desc']?>
+					<a href="<?php echo $jk_options['shoes_url']?>" target="_self">
+						<span>get shoes</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="bigsize-block content-block">
+				<img src="<?php echo $jk_options['bigsize_img']['url'] ?>" alt="" />
+				<div class="content-block-body">
+					<?php echo $jk_options['bigsize_desc']?>
+					<a href="<?php echo $jk_options['bigsize_url']?>" target="_self">
+						<span>get bigsize</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="touch-block content-block">
+				<img src="<?php echo $jk_options['touch_img']['url'] ?>" alt="" />
+				<div class="content-block-body">
+					<?php echo $jk_options['touch_desc']?>
+					<a href="<?php echo $jk_options['touch_url']?>" target="_self">
+						<span>get touch</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
     <?php get_footer(); ?>

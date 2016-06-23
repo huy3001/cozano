f7187e3d48b288bf42c8a52d92a4f87ae893ce81
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-06-23 18:09:02 +0700

    Style sidebar responsive

--- wp-content/themes/sutunam/woocommerce/single-product.php
+++ wp-content/themes/sutunam/woocommerce/single-product.php
@@ -22,6 +22,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header( 'shop' ); ?>
 <div class="left-content">
 	<?php sutunam_menu('left-menu'); ?>
+    <div class="login">
+        <?php if (is_user_logged_in()) { ?>
+            <a class="login-button" href="<?php echo wp_logout_url(home_url()); ?>">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <span><?php echo __('logout', 'sutunam') ?></span>
+            </a>
+        <?php } else { ?>
+            <a class="login-button show-login" id="show-login" href="#">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <span><?php echo __('login', 'sutunam') ?></span>
+            </a>
+        <?php } ?>
+    </div>
 </div>
 <div class="right-content">
 	<?php

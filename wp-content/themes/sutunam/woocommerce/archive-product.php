cb3c88fae0b8c49daa7bd09817f2d3a96f6b1ae5
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-07-22 16:18:15 +0700

    Fix product listing banner

--- wp-content/themes/sutunam/woocommerce/archive-product.php
+++ wp-content/themes/sutunam/woocommerce/archive-product.php
@@ -33,11 +33,25 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_before_main_content' );
 	?>
         <div class="cat-header">
-            <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+            <div class="cat-info">
+                <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
-                <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+                    <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
 
-            <?php endif; ?>
+                <?php endif; ?>
+                <?php
+                /**
+                 * woocommerce_archive_description hook.
+                 *
+                 * @hooked woocommerce_taxonomy_archive_description - 10
+                 * @hooked woocommerce_product_archive_description - 10
+                 */
+                do_action( 'woocommerce_archive_description' );
+                ?>
+
+                <?php if ( have_posts() ) : ?>
+            </div>
+
             <?php
             global $wp_query;
             $cat = $wp_query->get_queried_object();
@@ -50,18 +64,6 @@ get_header( 'shop' ); ?>
                 </div>
             <?php endif;?>
         </div>
-
-		<?php
-			/**
-			 * woocommerce_archive_description hook.
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
-			do_action( 'woocommerce_archive_description' );
-		?>
-
-		<?php if ( have_posts() ) : ?>
 
 			<?php
 				/**

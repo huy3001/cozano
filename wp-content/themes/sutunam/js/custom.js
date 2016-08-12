471e60fdd5613f07a614d7410c87186887755221
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-08-12 16:54:54 +0700

    Style filter responsive

--- wp-content/themes/sutunam/js/custom.js
+++ wp-content/themes/sutunam/js/custom.js
@@ -2,6 +2,7 @@
 (function($) {
     // Global variables
     var $desktop = 1200,
+        $laptop = 992,
         $tablet = 768;
 
     // Global functions
@@ -128,18 +129,50 @@
         filterProduct: function () {
             $.filtrify('product-category', 'placeHolder');
             setTimeout(function() {
-                var ftMenu = $('.ft-menu'),
+                var filterPanel = $('#placeHolder'),
+                    ftMenu = $('.ft-menu'),
                     ftLabel= $('.ft-label', ftMenu),
                     ftPanel = $('.ft-panel', ftMenu),
                     sortingBtn = $('.woocommerce-ordering .dropdown-toggle');
                 if(ftMenu.length) {
-                    ftLabel.on('click', function() {
-                        ftLabel.removeClass('ft-opened');
-                        ftPanel.addClass('ft-hidden');
-                        $(this).addClass('ft-opened');
-                        $(this).parents('.ft-field').find('.ft-panel').removeClass('ft-hidden');
-                        sortingBtn.parents('.bootstrap-select').removeClass('open');
-                    });
+                    if($(window).width() > $laptop - 1) {
+                        filterPanel.removeAttr('style');
+                        ftLabel.on('click', function() {
+                            ftLabel.removeClass('ft-opened');
+                            ftPanel.addClass('ft-hidden');
+                            $(this).addClass('ft-opened');
+                            $(this).parents('.ft-field').find('.ft-panel').removeClass('ft-hidden');
+                            sortingBtn.parents('.bootstrap-select').removeClass('open');
+                        });
+                    }
+                    else {
+                        var closeBtn = $('.btn-close'),
+                            filterBtn = $('.btn-filter');
+                        filterBtn.off('click').on('click', function() {
+                            closeBtn.fadeIn(300);
+                            filterPanel.fadeIn(300);
+                            $('body').css('overflow-y', 'hidden');
+                        });
+                        closeBtn.off('click').on('click', function() {
+                            $(this).fadeOut(300);
+                            filterPanel.fadeOut(300);
+                            $('body').css('overflow-y', 'auto');
+                        });
+                        ftLabel.off('click').on('click', function() {
+                            if($(this).hasClass('ft-clicked')) {
+                                $(this).removeClass('ft-clicked');
+                            }
+                            else {
+                                $(this).addClass('ft-clicked');
+                            }
+                            $(this).parent().siblings().find('.ft-clicked').removeClass('ft-clicked');
+                            $(this).parent().siblings().find('.ft-panel').slideUp(300);
+                            $(this).parent().find('.ft-panel').slideUp(300);
+                            if(!$(this).next('.ft-panel').is(':visible')) {
+                                $(this).next('.ft-panel').slideDown(300);
+                            }
+                        });
+                    }
                 }
                 if(sortingBtn.length) {
                     sortingBtn.on('click', function() {
@@ -198,5 +231,8 @@
 
         // Search toggle
         customJS.searchToggle();
+
+        // Filter product
+        customJS.filterProduct();
     });
 })(jQuery);

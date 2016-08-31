89841cd72adc40c57976d7013d35b2d960e8089d
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-08-31 15:53:05 +0700

    Remove error checking

--- wp-content/themes/sutunam/functions.php
+++ wp-content/themes/sutunam/functions.php
@@ -321,8 +321,7 @@ add_action( 'woocommerce_before_add_to_cart_button', 'add_content_before_addtoca
 
 function add_content_before_addtocart_button_func() {
     global $product;
-    $sizes = get_the_terms( get_the_ID(), 'size' );
-    var_dump($sizes);die;
+    $sizes = get_the_terms( get_the_ID(), 'color' );
     echo '<select>';
     echo '<option value="">size</option>';
     foreach ($sizes as $size){

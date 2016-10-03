90e4676d446c5c591928b8857c4bc6fc949d803d
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-10-03 15:13:00 +0700

    Style sub category

--- wp-content/themes/sutunam/woocommerce/archive-product.php
+++ wp-content/themes/sutunam/woocommerce/archive-product.php
@@ -58,7 +58,7 @@ get_header( 'shop' ); ?>
                 $link = get_term_link( $category->slug, $category->taxonomy );
                 $thumbnail = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
                 $image_sub = wp_get_attachment_url( $thumbnail );
-                echo '<li><a href="'. $link .'"><img src="'. $image_sub .'" /><p>'. $category->name .'</p></a></li>';
+                echo '<li><a href="'. $link .'"><img src="'. $image_sub .'" /><span>'. $category->name .'</span></a></li>';
             }
             echo '</ul></div>';
             ?>

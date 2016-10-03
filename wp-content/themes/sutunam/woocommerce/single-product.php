99a3b7d70c783f67dd415cf804b18a9e0f9a5a00
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-10-03 17:24:02 +0700

    Style sub category

--- wp-content/themes/sutunam/woocommerce/single-product.php
+++ wp-content/themes/sutunam/woocommerce/single-product.php
@@ -55,9 +55,9 @@ get_header( 'shop' ); ?>
 		$thumbnail = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
 		$image_sub = wp_get_attachment_url( $thumbnail );
         if($category->term_id == $cat->term_id)
-            echo '<li class="active"><a href="'. $link .'"><img src="'. $image_sub .'" /><span>'. $category->name .'</span></a></li>';
+            echo '<li class="active"><a href="'. $link .'"><figure><img src="'. $image_sub .'" /></figure><span>'. $category->name .'</span></a></li>';
         else
-            echo '<li><a href="'. $link .'"><img src="'. $image_sub .'" /><span>'. $category->name .'</span></a></li>';
+            echo '<li><a href="'. $link .'"><figure><img src="'. $image_sub .'" /></figure><span>'. $category->name .'</span></a></li>';
 	}
     echo '</ul></div>';
 	?>

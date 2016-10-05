028c860a80bbd1d93e8d2985789e2c0c45491198
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-10-05 16:47:56 +0700

    Fix blog page

--- wp-content/themes/sutunam/index.php
+++ wp-content/themes/sutunam/index.php
@@ -11,13 +11,15 @@
 
     <div class="content">
         <section id="main-content">
-            <?php if (have_posts()) : while (have_posts()) :the_post(); ?>
-                <?php get_template_part('content', get_post_format())?>
-            <?php endwhile; ?>
-            <?php sutunam_pagination(); ?>
-            <?php else: ?>
-                <?php get_template_part('content','none'); ?>
-            <?php endif; ?>
+            <div class="container">
+                <?php if (have_posts()) : while (have_posts()) :the_post(); ?>
+                    <?php get_template_part('content', get_post_format())?>
+                <?php endwhile; ?>
+                    <?php sutunam_pagination(); ?>
+                <?php else: ?>
+                    <?php get_template_part('content','none'); ?>
+                <?php endif; ?>
+            </div>
         </section>
         <section id="sidebar">
             <?php get_sidebar()?>

b67c3e9df4ed7c5dde02cbbed6874190d7d13590
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-10-06 12:01:05 +0700

    Fix layout product detail

--- wp-content/themes/sutunam/search.php
+++ wp-content/themes/sutunam/search.php
@@ -11,34 +11,32 @@ get_header(); ?>
 <div class="archive tax-product_cat term-t-shirts term-14 woocommerce woocommerce-page">
     <?php include(TEMPLATEPATH . '/left-content.php'); ?>
     <div class="right-content">
-        <div id="container">
-            <section id="primary" class="search-result-wrapper">
-                <div class="row">
-                    <div class="search-result-heading">
-                        <!--<h1 class="txt-title"><?php echo __('Search result', 'sutunam') ?></h1>-->
-                        <?php if (have_posts()) : ?>
-                            <?php
-                            global $wp_query;
-                            ?>
-                            <h1><?php printf(__('%s results for %s', 'sutunam'), $wp_query->found_posts, get_search_query()); ?></h1>
-                        <?php else: ?>
-                            <h1><?php echo __("There's no matched result for ", 'sutunam') . get_search_query() ?></h1>
-                        <?php endif ?>
-                    </div>
-                    <!-- search-result-heading -->
+        <section id="primary" class="search-result-wrapper">
+            <div class="row">
+                <div class="search-result-heading">
+                    <!--<h1 class="txt-title"><?php echo __('Search result', 'sutunam') ?></h1>-->
+                    <?php if (have_posts()) : ?>
+                        <?php
+                        global $wp_query;
+                        ?>
+                        <h1><?php printf(__('%s results for %s', 'sutunam'), $wp_query->found_posts, get_search_query()); ?></h1>
+                    <?php else: ?>
+                        <h1><?php echo __("There's no matched result for ", 'sutunam') . get_search_query() ?></h1>
+                    <?php endif ?>
                 </div>
-
-            </section><!-- .content-area -->
-
-            <div id="content" role="main" class="search-result-content">
-                <?php if (have_posts()) : ?>
-                    <ul id="product-category" class="products">
-                        <?php while (have_posts()) : the_post(); ?>
-                            <?php get_template_part('content', 'search'); ?>
-                        <?php endwhile; ?>
-                    </ul>
-                <?php endif ?>
+                <!-- search-result-heading -->
             </div>
+
+        </section><!-- .content-area -->
+
+        <div id="content" role="main" class="search-result-content">
+            <?php if (have_posts()) : ?>
+                <ul id="product-category" class="products">
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php get_template_part('content', 'search'); ?>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif ?>
         </div>
     </div>
 </div>

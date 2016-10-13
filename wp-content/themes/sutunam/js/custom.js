4c81a931d4f9ab18c22508139f6fbed2c1837df4
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2016-10-13 15:03:17 +0700

    Fix product deatil page

--- wp-content/themes/sutunam/js/custom.js
+++ wp-content/themes/sutunam/js/custom.js
@@ -140,7 +140,7 @@
         summaryCenter: function() {
             var summary = $('.product .summary');
             if(summary.length) {
-                if ($(window).width() > $laptop - 1) {
+                if ($(window).width() > $desktop - 1) {
                     summary.css('margin-top', -summary.outerHeight()/2);
                 }
                 else {
